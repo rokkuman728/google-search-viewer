3266e8de6118e07767722680ce0ec52a4089dd06
rokumotonatsumi <nekotatta723@gmail.com>
2023-03-30 08:54:04 +0900

    ui関連のライブラリインストール

--- resources/views/search_index.blade.php
+++ resources/views/search_index.blade.php
@@ -1,21 +1,32 @@
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>検索フォーム</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link href="cover.css" rel="stylesheet">
+</head>
+
+    {{-- CSS --}}
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{-- JS --}}
+    <script src="{{ asset('js/app.js') }}"></script>
+    {{-- popper.js CSS --}}
 </head>
 <body>
-    <h1>google検索フォーム</h1>
+<div class="alert alert-success" role="alert">
+google検索フォーム
 <div>
-    <form action="/search" method="POST">
-        @csrf
-        <input type="text" name="search_term">
-        <button type="submit">検索</button>
+
+<div class="input-group">
+<form action="/search" method="POST">
+    @csrf
+    <input type="text" class="form-control" name="search_term" placeholder="キーワードを入力">
+    <button class="btn btn-outline-success" type="submit" id="button-addon2"><i class="fas fa-search"></i> 検索</button>
+        {{-- <input type="text" name="search_term">
+        <button type="submit">検索</button> --}}
     </form>
 </div>
 </body>
-
-{{-- 
-<script async src="https://cse.google.com/cse.js?cx=35ae9abbb246b4f11">
-</script>
-<div class="gcse-search"></div> --}}
--- resources/views/search_results.blade.php
+++ resources/views/search_results.blade.php
@@ -1,11 +1,29 @@
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <title>検索結果一覧</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>検索結果一覧</title>
+  {{-- CSS --}}
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  {{-- JS --}}
+  <script src="{{ asset('js/app.js') }}"></script>
+  {{-- popper.js CSS --}}
+  <style>
+    #tooltip {
+      background: #333;
+      color: white;
+      font-weight: bold;
+      padding: 4px 8px;
+      font-size: 10px;
+      border-radius: 4px;
+    }
+  </style>
 </head>
 <body>
-    <h1>google検索結果一覧</h1>
+<div class="alert alert-success" role="alert">
+google検索結果一覧
 <div>
 
 @if (count($results) > 0)
