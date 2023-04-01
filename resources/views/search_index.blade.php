3581f721d8ecd22d56a1641f4167e6d680bcbdbe
rokumotonatsumi <nekotatta723@gmail.com>
2023-04-01 12:28:24 +0900

    リファクタリング

--- resources/views/search_index.blade.php
+++ resources/views/search_index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,21 +11,23 @@
     {{-- JS --}}
     <script src="{{ asset('js/app.js') }}"></script>
 </head>
+
 <body>
-<!-- main -->
-<div class="flex justify-center">
-    <div>
-        <img class="w-2/3 ml-auto mr-auto mt-10 mb-10" src="{{ url('images/icon0154.png') }}" alt="googleロゴ">
-    <form action="/search" method="POST">
-    <div class="flex border border-gray-200 rounded-full p-4 shadow text-xl">
-        <div>🔍</div>
-        @csrf
-        <input type="text" name="search_term" class="w-full outline-none px-3" placeholder="検索ワードを入力してください">
+    <!-- main -->
+    <div class="flex justify-center">
+        <div>
+            <img class="w-2/3 ml-auto mr-auto mt-10 mb-10" src="{{ url('images/icon0154.png') }}" alt="googleロゴ">
+            <form action="/search" method="POST">
+                <div class="flex border border-gray-200 rounded-full p-4 shadow text-xl">
+                    <div>🔍</div>
+                    @csrf
+                    <input type="text" name="search_term" class="w-full outline-none px-3" placeholder="検索ワードを入力してください">
+                </div>
+                <div class="mt-8 text-center">
+                    <button
+                        class="mr-3 bg-gray-200 border border-gray-300 py-3 px-4 rounded hover:bg-gray-400 hover:border-gray-500">検索</button>
+                </div>
+            </form>
+        </div>
     </div>
-    <div class="mt-8 text-center">
-        <button class="mr-3 bg-gray-200 border border-gray-300 py-3 px-4 rounded hover:bg-gray-400 hover:border-gray-500">検索</button>
-    </div>
-    </form>
-    </div>
-</div>
 </body>
